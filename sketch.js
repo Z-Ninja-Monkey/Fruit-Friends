d5eaae593e6255a327126ad47cd3c418c8b5c83a
Z-Ninja-Monkey <100244371+Z-Ninja-Monkey@users.noreply.github.com>
2023-02-13 20:34:24 +0000

    working on orange line

--- sketch.js
+++ sketch.js
@@ -11,9 +11,11 @@ let bobBanana;
 let bobTheBanana;
 let frame2;
 let bounce;
+let grapes;
 
 function preload(){
-	img5 = loadImage('assets/banana4.png');
+	img4 = loadImage('assets/orange.png');
+	img3 = loadImage('assets/banana4.png');
 	img2 = loadImage('assets/banana.png');
 	img = loadImage('assets/thingy5.png');
 }
@@ -37,11 +39,14 @@ function setup() {
 	platform = new Sprite(100,100, 100, 20, 'static');
 
 	img2.resize(150,0);
-	img5.resize(150,0);
+	img3.resize(150,0);
+	img4.resize(30,0);
 	
 	//img2.rotate = 123;
 	bobTheBanana = new Group();
-	BobTheBanana(90,400,0,true);
+	grapes = new Group();
+	BobTheBanana(100,400,0,false);
+	Grapes(50,300);
 
 }
 
@@ -52,6 +57,12 @@ function BobTheBanana(x,y,rotate,debug){
 	bobBanana.rotation = rotate; 
 	bobBanana.bounciness = 1;
 	bobBanana.debug = debug;
+}
+
+function Grapes(x,y){
+	grape = new grapes.Sprite(x,y, 40);
+	grape.collider = 'none';
+
 }
 
 function draw() {
@@ -82,28 +93,31 @@ function draw() {
 		bobTheBanana.img = img2
 		bobTheBanana[0].img.offset.y = 10;
 	} else if (bounce === 3){
-		bobTheBanana.img = img5;
+		bobTheBanana.img = img3;
 		bobTheBanana[0].img.offset.y = 10;
 	} else {
 		bobTheBanana.img = img2
 	}
+	bobTheBanana[0].img.offset.x = 10;
 	
-
+	if (thingy.overlaps(grapes)){
+		grapes.remove();
+	}
 
 	if (mouse.pressing()){
 		if (Math.abs((mouse.x - oldMouseX)) > 40 || Math.abs((mouse.y - oldMouseY)) > 40){
-			CreateLine(oldMouseX,oldMouseY,mouse.x,mouse.y,11);
+			CreateLine(oldMouseX,oldMouseY,mouse.x,mouse.y,11,false,'static',false);
 	    } else if ((Math.abs((mouse.x - oldMouseX)) < 40 && Math.abs((mouse.x - oldMouseX)) > 15) || (Math.abs((mouse.y - oldMouseY)) < 40) && Math.abs((mouse.y - oldMouseY)) > 15){
-			CreateLine(oldMouseX,oldMouseY,mouse.x,mouse.y,16);
+			CreateLine(oldMouseX,oldMouseY,mouse.x,mouse.y,16,false,'static',false);
 		} else if ((Math.abs((mouse.x - oldMouseX)) < 15 && Math.abs((mouse.x - oldMouseX)) > 2) || (Math.abs((mouse.y - oldMouseY)) < 15) && Math.abs((mouse.y - oldMouseY)) > 2){
-			CreateLine(oldMouseX,oldMouseY,mouse.x,mouse.y,30);
+			CreateLine(oldMouseX,oldMouseY,mouse.x,mouse.y,30,false,'static',false);
 		}
-		
+		CreateLine(oldMouseX,oldMouseY,mouse.x,mouse.y,100,true,'none',true);
 		
 	}
 	oldMouseX = mouse.x;
 	oldMouseY = mouse.y;
-	if (frame === 10){
+	if (frame === 100){
 		platform.collider = 'none';
 		platform.visible = false;
 		console.log('yes it was me that ran');
@@ -113,13 +127,12 @@ function draw() {
 	//console.log(frame);
 }
 
-function CreateLine(x,y,x2,y2,perc){
+function CreateLine(x,y,x2,y2,perc,visi,collide,tex){
 line = new Group();
 let moveAmount;
-line.visible = true;
-line.img = 'assets/thing.png';
-line.r =  10;
-line.collider = 'static';
+line.visible = visi;
+line.img = img4;
+line.r = 15;
 line.stroke = 'red';
 line.layer = 1;
 let spritething = new Sprite(0,0,0,0,0,'none');
@@ -136,14 +149,22 @@ spritething.visible = false;
 	spritething.x = x;
 	spritething.y = y;
 	len = 100 / perc;
-
+	lineNum = 0;
 	for (b = 0; b < len; b++) {
 		let linedot = new line.Sprite();
 		linedot.x = spritething.x;
 		linedot.y = spritething.y;
 		linedot.collider = 'static';
+		if (linedot.overlaps(line)){
+			linedot.remove();
+		}
+		linedot.collider = collide;
 		spritething.x += moveAmount[0];
 		spritething.y += moveAmount[1];
+		if (lineNum != 0){
+			lineNum += 1;
+		}
+		
 	}
 	
 }
